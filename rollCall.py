9de40a9de6778df49371b8f36b8003cd8ce9dd47
Tomas Torres <tomastorres003@hotmail.com>
2022-06-24 22:39:20 -0300

    update

--- rollCall.py
+++ rollCall.py
@@ -1,10 +1,10 @@
 from re import IGNORECASE
 from turtle import back
 import telebot
+from telebot import types
 from telebot.types import ReplyKeyboardMarkup
 from telebot.types import ReplyKeyboardRemove
 from config import API_KEY
-
 bot = telebot.TeleBot(API_KEY)
 
 chat={}
@@ -14,21 +14,17 @@ def roll_call_already_started(message):
     try:
         if chat[message.chat.id]['title']!='':
             bot.send_message(message.chat.id, f"Roll call with title {chat[message.chat.id]['title']} is still in progress")
-            contin=False
-            return contin
+            return False
     except:
-        contin=True
-        return contin
+        return True
 
 def roll_call_not_started(message):
     try:
         if chat[message.chat.id]['title']!='':
-            contin=True
-            return contin
+            return True
     except:
         bot.send_message(message.chat.id, "Roll call is not active")
-        contin=False
-        return contin
+        return False
 
 def create_txt_list(cid):
     chat[cid]['txtIn']=''
@@ -61,16 +57,46 @@ def create_txt_list(cid):
         else:
             chat[cid]['txtMaybe']+=f'{m}. {us}\n'
 
+def admin_rights(message):
+    try:
+        if len(chat[message.chat.id]['admins'])>0:
+            return True
+    except:
+        return False
+
+@bot.message_handler(func=lambda message:message.text.lower().split("@")[0]=="/set_admins" and bot.get_chat_member(message.chat.id, message.from_user.id).status=='admin')  # START COMMAND
+def set_admins(message):
+    try:
+        if chat[message.chat.id]:
+            pass
+    except:
+        chat[message.chat.id]={}
+   
+    admins=bot.get_chat_administrators(message.chat.id)
+    arrAdmins = []
+    for r in admins:
+        arrAdmins.append(r.user.id)
+    chat[message.chat.id]['admins']=arrAdmins
+
+@bot.message_handler(func=lambda message:message.text.lower().split("@")[0]=="/unset_admins" and bot.get_chat_member(message.chat.id, message.from_user.id).status=='admin')  # START COMMAND
+def unset_admins(message):
+    chat[message.chat.id]['admins']=[]
 
 @bot.message_handler(func=lambda message:message.text.lower().split("@")[0]=="/start")  # START COMMAND
 def welcome_and_explanation(message):
+    if admin_rights(message):
+        if bot.get_chat_member(message.chat.id,message.from_user.id).status not in ['admin', 'creator']:
+            bot.send_message(message.chat.id, "Error - user does not have sufficient permissions for this operation")
+            return
+
+    chat[message.chat.id] = {}
     markup = ReplyKeyboardMarkup(row_width=3)         
-    markup.add('/start_roll_call', '/in', '/out', '/maybe', '/whos_in', '/end_roll_call')        
+   # markup.add('/start_roll_call', '/in', '/out', '/maybe', '/whos_in', '/end_roll_call')    
+    markup.add('/in', '/out', '/maybe', '/whos_in', '/whos_out', '/whos_maybe', '/shh', '/louder', '/start_roll_call', '/end_roll_call', '/help')    
     bot.send_message(message.chat.id, '''
 Hi! im RollCall!
 Type /help to see all the commands
     ''', reply_markup=markup)
-
 
 @bot.message_handler(func=lambda message:message.text.lower().split("@")[0]=="/help")  # HELP COMMAND
 def welcome_and_explanation(message):
@@ -82,23 +108,26 @@ The commands are:
 -/in || To let everybody know you will be attending (optional comment)
 -/out || To let everybody know you wont be attending (optional comment)
 -/maybe  || To let everybody know you dont know (optional comment)
--/whos_in || list attendees
--/whos_out || list attendees
--/whos_maybe || list attendees
+-/whos_in || List of those who will go
+-/whos_out || List of those who will not go
+-/whos_maybe || List of those who maybe will go
 -/set_title "title" || To set a title for the current roll call
 -/set_in_for "name" || Allows you to respond for another user
 -/set_out_for "name" || Allows you to respond for another user
 -/set_maybe_for "name" || Allows you to respond for another user
 -/shh || to apply minimum output for each command
 -/louder || to disable minimum output for each command
--/end_roll_call   || to end a roll call
+-/end_roll_call   || To end a roll call
     ''')
 @bot.message_handler(func=lambda message:message.text.lower().split("@")[0]=="/start_roll_call")  # START ROLL CALL COMMAND
 def start_roll_call(message):
 
-    contin=roll_call_already_started(message)
+    if admin_rights(message):
+        if bot.get_chat_member(message.chat.id,message.from_user.id).status not in ['admin', 'creator']:
+            bot.send_message(message.chat.id, "Error - user does not have sufficient permissions for this operation")
+            return
 
-    if contin==True:
+    if roll_call_already_started(message):
         cid = message.chat.id  # chat id
         msg = message.text
         arr = msg.split(" ")
@@ -111,7 +140,6 @@ def start_roll_call(message):
         if title == '':
             title = '<Empty>'  # if title is empty default title is Roll name
 
-        chat[cid] = {}
         chat[cid]['txtIn']='Nobody'
         chat[cid]['txtOut']='Nobody'
         chat[cid]['txtMaybe']='Nobody'
@@ -123,23 +151,23 @@ def start_roll_call(message):
         chat[cid]['shh']=False
 
         bot.send_message(message.chat.id, "Roll call with title: "+title+" started!")
-        print("A roll call with title "+title+"has started")
+        print("A roll call with title "+title+" has started")
 
 @bot.message_handler(func=lambda message:message.text.lower().split("@")[0]=="/shh")
 def shh(message):
-    chat[message.chat.id]['shh']=True
-    bot.send_message(message.chat.id, "Ok, i will keep quiet!")
+    if roll_call_not_started(message):
+        chat[message.chat.id]['shh']=True
+        bot.send_message(message.chat.id, "Ok, i will keep quiet!")
 
 @bot.message_handler(func=lambda message:message.text.lower().split("@")[0]=="/louder")
 def louder(message):
-    chat[message.chat.id]['shh']=False
-    bot.send_message(message.chat.id, "Ok, i can hear you!")
+    if roll_call_not_started(message):
+        chat[message.chat.id]['shh']=False
+        bot.send_message(message.chat.id, "Ok, i can hear you!")
 
 @bot.message_handler(func=lambda message:(message.text.split(" "))[0].split("@")[0].lower() == "/in")  # IN COMMAND
 def in_user(message):
-    contin=roll_call_not_started(message)
-
-    if contin==True:
+    if roll_call_not_started(message):
         condition=True
         msg = message.text
         cid = message.chat.id  # chat id
@@ -189,12 +217,7 @@ f"""Title - {chat[cid]['title']}:\n{'In:'+backslash+chat[cid]['txtIn']+backslash
 
 @bot.message_handler(func=lambda message:(message.text.split(" "))[0].split("@")[0].lower() == "/out")  # OUT COMMAND
 def out_user(message):
-    try:
-        if chat[message.chat.id]['title']!='':
-            pass
-    except:
-        bot.send_message(message.chat.id, "Roll call is not active")
-    else:
+    if roll_call_not_started(message):
         condition=True
         comment = ''
         msg = message.text
@@ -245,13 +268,7 @@ f"""Title - {chat[cid]['title']}:\n{'In:'+backslash+chat[cid]['txtIn']+backslash
 
 @bot.message_handler(func=lambda message:(message.text.split(" "))[0].split("@")[0].lower() == "/maybe")  # MAYBE COMMAND
 def maybe_user(message):
-    try:
-        if chat[message.chat.id]['title']!='':
-            contin=True
-    except:
-        bot.send_message(message.chat.id, "Roll call is not active")
-        contin=-False
-    if contin==True:
+    if roll_call_not_started(message):
         condition=True
         comment=''
         msg = message.text
@@ -302,13 +319,7 @@ f"""Title - {chat[cid]['title']}:\n{'In:'+backslash+chat[cid]['txtIn']+backslash
 
 @bot.message_handler(func=lambda message:message.text.lower().split("@")[0]=="/whos_in")  # WHOS IN COMMAND
 def whos_in(message):
-    try:
-        if chat[message.chat.id]['title']!='':
-            condition=True
-    except:
-        bot.send_message(message.chat.id, "Roll call is not active")
-        condition=False
-    if condition==True:
+    if roll_call_not_started(message):
         cid = message.chat.id
         
         if len(chat[cid]['usersAttendance'])==0:
@@ -321,24 +332,17 @@ def whos_in(message):
                 print(i)
                 if us in chat[cid]['usersComments']:
                     comment=chat[cid]['usersComments'][us]
-                    chat[cid]['txtIn']+=f'{i}.{us} ({comment})\n'
+                    chat[cid]['txtIn']+=f'{i}. {us} ({comment})\n'
                 else:
-                    chat[cid]['txtIn']+=f'{i}.{us}\n'
+                    chat[cid]['txtIn']+=f'{i}. {us}\n'
         bot.send_message(message.chat.id, f"""
 In:
 {chat[cid]['txtIn']}""")  # list of who will attendance
 
-
 @bot.message_handler(func=lambda message:message.text.lower().split("@")[0]=="/whos_out")  # WHOS IN COMMAND
 def whos_out(message):
     cid = message.chat.id
-    try:
-        if chat[message.chat.id]['title']!='':
-            contin=True
-    except:
-        bot.send_message(message.chat.id, "Roll call is not active")
-        contin=False
-    if contin==True:
+    if roll_call_not_started(message):
         if len(chat[cid]['usersNotAttendance'])==0:
             chat[cid]['txtOut']='Nobody'
         else:
@@ -348,25 +352,18 @@ def whos_out(message):
                 o+=1
                 if us in chat[cid]['usersComments']:
                     comment=chat[cid]['usersComments'][us]
-                    chat[cid]['txtOut']+=f'{o}.{us} ({comment})\n'
+                    chat[cid]['txtOut']+=f'{o}. {us} ({comment})\n'
                 else:
-                    chat[cid]['txtOut']+=f'{o}.{us}\n'
+                    chat[cid]['txtOut']+=f'{o}. {us}\n'
         
         bot.send_message(message.chat.id, f"""
 Out:
 {chat[cid]['txtOut']}""")  # list of who will not attendance
 
-
 @bot.message_handler(func=lambda message:message.text.lower().split("@")[0]=="/whos_maybe")  # WHOS IN COMMAND
 def whos_maybe(message):
     cid = message.chat.id
-    try:
-        if chat[message.chat.id]['title']!='':
-            contin=True
-    except:
-        bot.send_message(message.chat.id, "Roll call is not active")
-        contin=False
-    if contin==True:
+    if roll_call_not_started(message):
         if len(chat[cid]['usersMaybeAttendance'])==0:
             chat[cid]['txtMaybe']='Nobody'
         else:
@@ -376,9 +373,9 @@ def whos_maybe(message):
                 m+=1
                 if us in chat[cid]['usersComments']:
                     comment=chat[cid]['usersComments'][us]
-                    chat[cid]['txtMaybe']+=f'{m}.{us} ({comment})\n'
+                    chat[cid]['txtMaybe']+=f'{m}. {us} ({comment})\n'
                 else:
-                    chat[cid]['txtMaybe']+=f'{m}.{us}\n'
+                    chat[cid]['txtMaybe']+=f'{m}. {us}\n'
         bot.send_message(message.chat.id, f"""
 Maybe:
 {chat[cid]['txtMaybe']}""")  # list of who will maybe attendance
@@ -386,14 +383,7 @@ Maybe:
 @bot.message_handler(func=lambda message:(message.text.split(" "))[0].split("@")[0].lower() == "/set_in_for")
 @bot.message_handler(func=lambda message:(message.text.split(" "))[0].split("@")[0].lower() == "/sif")
 def set_in_for(message):
-    try:
-        if chat[message.chat.id]['title']!='':
-            contin=True
-    except:
-        bot.send_message(message.chat.id, "Roll call is not active")
-        contin=False
-
-    if contin==True:
+    if roll_call_not_started(message):
         condition=True
         cid = message.chat.id
         msg = message.text
@@ -451,14 +441,7 @@ f"""Title - {chat[cid]['title']}:\n{'In:'+backslash+chat[cid]['txtIn']+backslash
 @bot.message_handler(func=lambda message:(message.text.split(" "))[0].split("@")[0].lower() == "/set_out_for")
 @bot.message_handler(func=lambda message:(message.text.split(" "))[0].split("@")[0].lower() == "/sof")
 def set_out_for(message):
-    try:
-        if chat[message.chat.id]['title']!='':
-            contin=True
-    except:
-        bot.send_message(message.chat.id, "Roll call is not active")
-        contin=False
-
-    if contin==True:
+    if roll_call_not_started(message):
         condition=True
         cid = message.chat.id
         msg = message.text
@@ -514,14 +497,7 @@ f"""Title - {chat[cid]['title']}:\n{'In:'+backslash+chat[cid]['txtIn']+backslash
 @bot.message_handler(func=lambda message:(message.text.split(" "))[0].split("@")[0].lower() == "/set_maybe_for")
 @bot.message_handler(func=lambda message:(message.text.split(" "))[0].split("@")[0].lower() == "/smf")
 def set_maybe_for(message):
-    try:
-        if chat[message.chat.id]['title']!='':
-            contin=True
-    except:
-        bot.send_message(message.chat.id, "Roll call is not active")
-        contin=False
-
-    if contin==True:
+    if roll_call_not_started(message):
         condition=True
         cid = message.chat.id
         msg = message.text
@@ -576,13 +552,7 @@ def set_maybe_for(message):
 
 @bot.message_handler(func=lambda message:(message.text.split(" "))[0].split("@")[0].lower()=="/set_title")  # SET TITLE COMMAND
 def set_title(message):
-    try:
-        if chat[message.chat.id]['title']!='':
-            contin=True
-    except:
-        bot.send_message(message.chat.id, "Roll call is not active")
-        contin=False
-    if contin==True:
+    if roll_call_not_started(message):
         cid = message.chat.id
         msg = message.text
         arr = msg.split(" ")
@@ -607,8 +577,14 @@ def unknowCommand(message):
 
 @bot.message_handler(func=lambda message:message.text.lower()=="/end_roll_call")  #START ROLL CALL COMMAND
 def end_roll_call(message):
-    contin=roll_call_not_started(message)
-    if contin==True:
+
+    if admin_rights(message):
+        if bot.get_chat_member(message.chat.id,message.from_user.id).status not in ['admin', 'creator']:
+            bot.send_message(message.chat.id, "Error - user does not have sufficient permissions for this operation")
+            print(admin_rights(message))
+            return
+
+    if roll_call_not_started(message):
         
         cid=message.chat.id
         chat['count']=0

ffe470dc60b670f1e137e53db773791fd781d781
Tomas Torres <tomastorres003@hotmail.com>
2022-08-20 20:34:01 -0300

    quick fix

--- rollCall/runner.py
+++ rollCall/runner.py
@@ -18,4 +18,5 @@ async def main():
     logging.info("Bot shutting down.")
 
 if __name__=="__main__":
-    asyncio.run(main())
+    loop=asyncio.get_event_loop()
+    loop.run_until_complete(main())

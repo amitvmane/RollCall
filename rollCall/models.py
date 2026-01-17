56fae35646db3ab25fd8804df3d178327540d29d
amitvmane <amitvmane@gmail.com>
2026-01-17 23:44:06 +0530

    Fixed issue with moving out existing IN user

--- rollCall/models.py
+++ rollCall/models.py
@@ -1,24 +1,19 @@
 import logging
 import asyncio
 import telebot
-
 from config import TELEGRAM_TOKEN
 from exceptions import *
 from functions import *
 from datetime import datetime
-#from database import RollCallDatabase
 import db
 import re
 import traceback
 
 bot = telebot.TeleBot(token=TELEGRAM_TOKEN)
 
-# Initialize database connection
-#db = RollCallDatabase('rollcall.db')
-
 class RollCall:
     """RollCall object with database persistence"""
-
+    
     def __init__(self, title, chat_id=None, db_id=None):
         """
         Initialize RollCall either from database (db_id) or create new (chat_id + title)
@@ -49,11 +44,10 @@ class RollCall:
             else:
                 self.id = None
                 self.chat_id = None
-
+    
     def _load_from_db(self, db_id):
         """Load rollcall data from database"""
         data = db.get_rollcall(db_id)
-        
         if not data:
             raise Exception(f"RollCall with ID {db_id} not found")
         
@@ -71,7 +65,7 @@ class RollCall:
             self.finalizeDate = datetime.fromisoformat(data['finalize_date'])
         else:
             self.finalizeDate = None
-        
+            
         if data['created_at']:
             self.createdDate = datetime.fromisoformat(data['created_at'])
         else:
@@ -79,7 +73,7 @@ class RollCall:
         
         # Load users from database
         self._load_users_from_db()
-
+    
     def _load_users_from_db(self):
         """Load all users from database"""
         self.inList = []
@@ -90,7 +84,6 @@ class RollCall:
         
         # Load regular users
         all_users_data = db.get_all_users(self.id)
-        
         for user_data in all_users_data:
             user = User.__new__(User)
             user.user_id = user_data['user_id']
@@ -114,7 +107,6 @@ class RollCall:
         # Load proxy users
         for status in ['in', 'out', 'maybe', 'waitlist']:
             proxy_users_data = db.get_proxy_users_by_status(self.id, status)
-            
             for proxy_data in proxy_users_data:
                 user = User.__new__(User)
                 user.user_id = proxy_data['name']  # String ID for proxy users
@@ -133,7 +125,7 @@ class RollCall:
                     self.waitList.append(user)
                 
                 self.allNames.append(user)
-
+    
     def save(self):
         """Save current rollcall state to database"""
         if self.id:
@@ -147,7 +139,7 @@ class RollCall:
                 event_fee=self.event_fee,
                 in_list_limit=self.inListLimit
             )
-
+    
     # RETURN INLIST
     def inListText(self):
         txt = f'In:\n'
@@ -163,7 +155,7 @@ class RollCall:
         i = 0
         for user in self.outList:
             i += 1
-            txt += f"{i}. {user}\n" 
+            txt += f"{i}. {user}\n"
         return txt + '\n' if len(self.outList) > 0 else "Out:\nNobody\n\n"
     
     # RETURN MAYBELIST
@@ -172,40 +164,37 @@ class RollCall:
         i = 0
         for user in self.maybeList:
             i += 1
-            txt += f"{i}. {user}\n" 
+            txt += f"{i}. {user}\n"
         return txt + '\n' if len(self.maybeList) > 0 else "Maybe:\nNobody\n\n"
-
+    
     # RETURN WAITLIST
     def waitListText(self):
         txt = f'Waiting:\n'
         i = 0
         for user in self.waitList:
             i += 1
-            txt += f"{i}. {user}\n" 
+            txt += f"{i}. {user}\n"
         return (txt + '\n' if len(self.waitList) > 0 else "Waiting:\nNobody") if self.inListLimit != None else ""
-
+    
     # RETURN ALL THE STATES
     def allList(self):
         try:
             _datetime = self.finalizeDate.strftime('%d-%m-%Y %H:%M')
         except:
             _datetime = 'Yet to decide'
-
         txt = "Title: " + self.title + '\nID: ' + "__RCID__" + f"\nEvent time: {_datetime} {self.timezone if _datetime != 'Yet to decide' else ''}\nLocation: {self.location if self.location != None else 'Yet to decide'}\n\n" + (self.inListText() if self.inListText() != 'In:\nNobody\n\n' else '') + (self.outListText() if self.outListText() != 'Out:\nNobody\n\n' else '') + (self.maybeListText() if self.maybeListText() != 'Maybe:\nNobody\n\n' else '') + (self.waitListText() if self.waitListText() != 'Waiting:\nNobody' else '') + 'Max limit: ' + ('∞' if self.inListLimit == None else str(self.inListLimit))
         return txt
-
+    
     # RETURN THE FINISH LIST (ONLY IN ERC COMMAND)
     def finishList(self):
         try:
             _datetime = self.finalizeDate.strftime('%d-%m-%Y %H:%M')
         except:
             _datetime = ''
-
         backslash = '\n'
         txt = "Title: " + self.title + '\nID: ' + '__RCID__' + f"{(backslash + 'Event time: ' + _datetime + ' ' + self.timezone) if _datetime != '' else ''}{(backslash + 'Location:' + self.location) if self.location != None else ''}{(backslash + 'Event Fee: ' + str(self.event_fee)) if self.event_fee != None else backslash * 2 + 'In case of paid event - reach out to organiser for payment contribution'}{(backslash + 'Individual Fee: ' + str((round(int(re.sub(r'[^0-9]', '', self.event_fee)) / len(self.inList), 2)) if len(self.inList) > 0 else '0')) if self.event_fee != None else ''}\n\n" + ("Additional unknown/penalty fees are not included and needs to be handled separately.\n\n" if self.event_fee != None else '') + (self.inListText() if self.inListText() != 'In:\nNobody\n\n' else 'In:\nNobody\n\n') + (self.outListText() if self.outListText() != 'Out:\nNobody\n\n' else 'Out:\nNobody\n\n') + (self.maybeListText() if self.maybeListText() != 'Maybe:\nNobody\n\n' else 'Maybe:\nNobody\n\n') + (self.waitListText() if self.waitListText() != 'Waiting:\nNobody' else '') + 'Max limit: ' + ('∞' if self.inListLimit == None else str(self.inListLimit))
-        
         return txt
-
+    
     # DELETE A USER
     def delete_user(self, name):
         """Delete a user by name from database and memory"""
@@ -219,21 +208,36 @@ class RollCall:
         except:
             print(traceback.format_exc())
             return False
-
+    
     # ADD A NEW USER TO IN LIST
     def addIn(self, user):
         print(self.allNames)
-
-        # ERROR FOR REPEATLY NAME IN SET COMMANDS
-        if type(user.user_id) == str: 
+        
+        # Check if this is a proxy command trying to move a real user
+        if type(user.user_id) == str:
+            # Find if a real user with this name exists
+            real_user_found = None
             for us in self.allNames:
-                if user.name == us.name and user.user_id != us.user_id:
-                    return 'AA'
-
-        for us in self.allNames:
-            if us.first_name == user.first_name and us.username == user.username and us.user_id != user.user_id:
-                return "AB"
-
+                if (us.name == user.name or us.first_name == user.name) and type(us.user_id) == int:
+                    # Real user found! Use their object instead
+                    real_user_found = us
+                    break
+            
+            # If real user exists, use them instead of creating proxy
+            if real_user_found:
+                user = real_user_found
+                # Preserve any new comment from the proxy command
+                # (comment is already set in telegram_helper.py)
+            else:
+                # Only check for duplicate proxy if NO real user exists
+                for us in self.allNames:
+                    if user.name == us.name and user.user_id != us.user_id:
+                        return 'AA'
+                
+                for us in self.allNames:
+                    if us.first_name == user.first_name and us.username == user.username and us.user_id != user.user_id:
+                        return "AB"
+        
         # ERROR FOR DUPLICATE USER IN THE SAME STATE
         if self.inListLimit == None:
             for us in self.inList:
@@ -256,7 +260,7 @@ class RollCall:
                     us.comment = user.comment
                     self._save_user_to_db(user, 'in')
                     return
-
+                    
             for us in self.waitList:
                 if us.user_id == user.user_id and us.comment == user.comment:
                     return "AB"
@@ -264,53 +268,71 @@ class RollCall:
                     us.comment = user.comment
                     self._save_user_to_db(user, 'waitlist')
                     return
-
+        
         # REMOVE THE USER FROM OTHER STATES
-        for us in self.outList:
+        for us in self.outList[:]:
             if us.user_id == user.user_id:
                 self.outList.remove(us)
-                self.allNames.remove(us)
-
-        for us in self.maybeList:
+                if us in self.allNames:
+                    self.allNames.remove(us)
+                    
+        for us in self.maybeList[:]:
             if us.user_id == user.user_id:
                 self.maybeList.remove(us)
-                self.allNames.remove(us)
-
-        for us in self.waitList:
+                if us in self.allNames:
+                    self.allNames.remove(us)
+                    
+        for us in self.waitList[:]:
             if us.user_id == user.user_id:
                 self.waitList.remove(us)
-                self.allNames.remove(us)
-
+                if us in self.allNames:
+                    self.allNames.remove(us)
+        
         # WAITLIST FEATURE
         if self.inListLimit != None:
-            if len(self.inList) == int(self.inListLimit):
+            if len(self.inList) >= int(self.inListLimit):
                 self.waitList.append(user)
-                self.allNames.append(user)
+                if user not in self.allNames:
+                    self.allNames.append(user)
                 self._save_user_to_db(user, 'waitlist')
                 logging.info(f"The user {user.name} has been added to the Wait list")
                 return 'AC'
-
+        
         # ADD THE USER TO THE STATE
         self.inList.append(user)
-        self.allNames.append(user)
+        if user not in self.allNames:
+            self.allNames.append(user)
         self._save_user_to_db(user, 'in')
-
         logging.info(f"User {user.name} has change his state to in")
-
+    
     # ADD A NEW USER TO OUT LIST
     def addOut(self, user):
         print(self.allNames)
-
-        # ERROR FOR REPEATLY NAME IN SET COMMANDS
-        if type(user.user_id) == str: 
+        
+        # Check if this is a proxy command trying to move a real user
+        if type(user.user_id) == str:
+            # Find if a real user with this name exists
+            real_user_found = None
             for us in self.allNames:
-                if user.name == us.name and user.user_id != us.user_id:
-                    return 'AA'
-
-        for us in self.allNames:
-            if us.first_name == user.first_name and us.username == user.username and us.user_id != user.user_id:
-                return "AB"
-
+                if (us.name == user.name or us.first_name == user.name) and type(us.user_id) == int:
+                    # Real user found! Use their object instead
+                    real_user_found = us
+                    break
+            
+            # If real user exists, use them instead of creating proxy
+            if real_user_found:
+                user = real_user_found
+                # Preserve any new comment from the proxy command
+            else:
+                # Only check for duplicate proxy if NO real user exists
+                for us in self.allNames:
+                    if user.name == us.name and user.user_id != us.user_id:
+                        return 'AA'
+                
+                for us in self.allNames:
+                    if us.first_name == user.first_name and us.username == user.username and us.user_id != user.user_id:
+                        return "AB"
+        
         # ERROR FOR DUPLICATE USER IN THE SAME STATE
         for us in self.outList:
             if us.user_id == user.user_id and us.comment == user.comment:
@@ -321,56 +343,73 @@ class RollCall:
                 us.comment = user.comment
                 self._save_user_to_db(user, 'out')
                 return
-
+        
         # REMOVE FROM OTHER STATES
-        for us in self.inList:
+        for us in self.inList[:]:
             if us.user_id == user.user_id:
                 self.inList.remove(us)
-                self.allNames.remove(us)
-
-        for us in self.maybeList:
+                if us in self.allNames:
+                    self.allNames.remove(us)
+                    
+        for us in self.maybeList[:]:
             if us.user_id == user.user_id:
                 self.maybeList.remove(us)
-                self.allNames.remove(us)
-
+                if us in self.allNames:
+                    self.allNames.remove(us)
+        
         if self.inListLimit != None:
-            for us in self.waitList:
+            for us in self.waitList[:]:
                 if us.user_id == user.user_id:
                     self.waitList.remove(us)
-                    self.allNames.remove(us)
-
-        if self.inListLimit != None:
+                    if us in self.allNames:
+                        self.allNames.remove(us)
+                        
             if len(self.inList) < int(self.inListLimit) and len(self.waitList) > 0:
                 result = self.waitList[0]
                 self.inList.append(self.waitList[0])
                 self.waitList.pop(0)
-                self.outList.append(user)  
-                self.allNames.append(user)
+                self.outList.append(user)
+                if user not in self.allNames:
+                    self.allNames.append(user)
                 self._save_user_to_db(result, 'in')
                 self._save_user_to_db(user, 'out')
                 logging.info(f"User {user.name} has change his state to out")
                 return result
-
-        self.outList.append(user)  
-        self.allNames.append(user)
+        
+        self.outList.append(user)
+        if user not in self.allNames:
+            self.allNames.append(user)
         self._save_user_to_db(user, 'out')
-
         logging.info(f"User {user.name} has change his state to out")
-
+    
     # ADD A NEW USER TO MAYBE LIST
     def addMaybe(self, user):
         print(self.allNames)
-
-        # ERROR FOR REPEATLY NAME IN SET COMMANDS
-        if type(user.user_id) == str: 
+        
+        # Check if this is a proxy command trying to move a real user
+        if type(user.user_id) == str:
+            # Find if a real user with this name exists
+            real_user_found = None
             for us in self.allNames:
-                if user.name == us.name and user.user_id != us.user_id:
-                    return 'AA'
-
-        for us in self.allNames:
-            if us.first_name == user.first_name and us.username == user.username and us.user_id != user.user_id:
-                return "AB"
-
+                if (us.name == user.name or us.first_name == user.name) and type(us.user_id) == int:
+                    # Real user found! Use their object instead
+                    real_user_found = us
+                    break
+            
+            # If real user exists, use them instead of creating proxy
+            if real_user_found:
+                user = real_user_found
+                # Preserve any new comment from the proxy command
+            else:
+                # Only check for duplicate proxy if NO real user exists
+                for us in self.allNames:
+                    if user.name == us.name and user.user_id != us.user_id:
+                        return 'AA'
+                
+                for us in self.allNames:
+                    if us.first_name == user.first_name and us.username == user.username and us.user_id != user.user_id:
+                        return "AB"
+        
         # ERROR FOR DUPLICATE USER IN THE SAME STATE
         for us in self.maybeList:
             if us.user_id == user.user_id and us.comment == user.comment:
@@ -379,80 +418,82 @@ class RollCall:
                 us.comment = user.comment
                 self._save_user_to_db(user, 'maybe')
                 return
-
+        
         # REMOVE FROM OTHER STATES
-        for us in self.outList:
+        for us in self.outList[:]:
             if us.user_id == user.user_id:
                 self.outList.remove(us)
-                self.allNames.remove(us)
-
-        for us in self.inList:
+                if us in self.allNames:
+                    self.allNames.remove(us)
+                    
+        for us in self.inList[:]:
             if us.user_id == user.user_id:
                 self.inList.remove(us)
-                self.allNames.remove(us)
-
+                if us in self.allNames:
+                    self.allNames.remove(us)
+        
         if self.inListLimit != None:
-            for us in self.waitList:
+            for us in self.waitList[:]:
                 if us.user_id == user.user_id:
                     self.waitList.remove(us)
-                    self.allNames.remove(us)
-
-        if self.inListLimit != None:
+                    if us in self.allNames:
+                        self.allNames.remove(us)
+                        
             if len(self.inList) < int(self.inListLimit) and len(self.waitList) > 0:
                 result = self.waitList[0]
                 self.inList.append(self.waitList[0])
                 self.waitList.pop(0)
                 self.maybeList.append(user)
-                self.allNames.append(user)
+                if user not in self.allNames:
+                    self.allNames.append(user)
                 self._save_user_to_db(result, 'in')
                 self._save_user_to_db(user, 'maybe')
                 logging.info(f"User {user.name} has change his state to maybe")
                 return result
-
+        
         self.maybeList.append(user)
-        self.allNames.append(user)
+        if user not in self.allNames:
+            self.allNames.append(user)
         self._save_user_to_db(user, 'maybe')
-
         logging.info(f"User {user.name} has change his state to maybe")
-
+    
     def _save_user_to_db(self, user, status):
         """Save user to database"""
         if type(user.user_id) == int:
             # Regular user
             db.add_or_update_user(
-                self.id, 
-                user.user_id, 
-                user.first_name, 
-                user.username, 
-                status, 
+                self.id,
+                user.user_id,
+                user.first_name,
+                user.username,
+                status,
                 user.comment
             )
         else:
             # Proxy user
             db.add_or_update_proxy_user(
-                self.id, 
-                user.name, 
-                status, 
+                self.id,
+                user.name,
+                status,
                 user.comment
             )
 
 
 class User:
     """USER OBJECT"""
-
     def __init__(self, name, username, user_id, allNames):
         self.name = name
         self.first_name = name
         self.username = username
         self.user_id = user_id
         self.comment = ''
-
+        
         # ADD USERNAMES TO NAMES IN NORMAL COMMANDS
         if type(self.user_id) == int:
             for user in allNames:
                 if self.name == user.name and self.user_id != user.user_id:
                     self.name = f"{self.name} ({self.username})"
-
+    
     def __str__(self):
         backslash = "\n"
         return f"{self.name + (' (' + self.comment + ')' if self.comment != '' else '')}"
